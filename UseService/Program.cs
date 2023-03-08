185d1cc21711220de43521ef41bb39a7ff9855f3
Alexander Fedorov <alexander_fedorov1971@mail.ru>
2023-03-08 20:27:14 +0300

    use host environment

---
+++ UseService/Program.cs
@@ -0,0 +1,22 @@
+namespace UseService
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var app = builder.Build();
+
+			app.MapGet("/", async context =>
+			{
+				context.Response.ContentType = "text/html;charset=utf-8";
+
+			    var env = app.Services.GetService<IHostEnvironment>();
+
+				await context.Response.WriteAsync($"Привет из приложения {env!.ApplicationName}, среда {env.EnvironmentName}");
+			});
+
+			app.Run();
+		}
+	}
+}
