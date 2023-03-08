93efbc91413861faaeaa7581f218bc2267374b7f
Alexander Fedorov <alexander_fedorov1971@mail.ru>
2023-03-09 00:35:22 +0300

    fix favicon.ico

--- SingletonService/Middleware/SetContentTypeMiddleware.cs
+++ SingletonService/Middleware/SetContentTypeMiddleware.cs
@@ -11,6 +11,8 @@
 		public async Task InvokeAsync(HttpContext context)
 		{
 			context.Response.ContentType = "text/html;charset=utf-8";
+			// Избавляемся от запроса к favicon.ico
+			await context.Response.WriteAsync("<head><link rel='icon' href='data:,'/></head>");
 			await _next(context);
 		}
 	}
